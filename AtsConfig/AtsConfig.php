e0b418f4cbf69e35fe15bcd9b5540ce28f4be021
HMinng <jackhm18@gmail.com>
2014-08-04 16:44:39 +0800

    update

--- AtsConfig/AtsConfig.php
+++ AtsConfig/AtsConfig.php
@@ -5,30 +5,20 @@ use HMinng\ObjectGenerator\ObjectGenerator;
 class AtsConfig extends ObjectGenerator
 {
 	private $urls = array(
-		// 基础数据
-		'BasicData' => 'http://api.basicdata.baicheng.com/',
-		// 供应商系统
-		'SupplierManagement' => 'http://api.supplier.baicheng.com/',
-		// 政策
-		'VisaFare' => 'http://192.168.3.154:11002/',
-		// 订单
-		'VisaOrder' => 'http://192.168.3.154:11003/',
-		// 支付
-		'PayCenter' => 'http://192.168.3.154:11015/',
-		// 引擎
-		'VisaEngine' => 'http://192.168.3.154:11007/',
-		// 客服
-		'CustomerService' => 'http://192.168.3.154:11014/',
-		// cms
-		'ConfigManageSystem' => 'http://192.168.3.154:11016/',
-		// 物流
-		'TransferSystem' => 'http://192.168.3.154:11018/',
-		// 推送
-		'AtsPush'   => 'http://192.168.3.207:80/',
-		// 会员
-		'Passport' => 'http://apippt.baicheng.com/',
-		// 问答
-		'WenDa' => 'http://wenda.baicheng.com/',
+		'BasicData' 			=> 'http://api.basicdata.baicheng.com/', 			// 基础数据
+		'SupplierManagement' 	=> 'http://api.supplier.baicheng.com/',				//供应商系统
+		'VisaFare' 				=> 'http://192.168.3.154:11002/',					//政策
+		'VisaOrder' 			=> 'http://192.168.3.154:11003/',					//订单
+		'PayCenter' 			=> 'http://192.168.3.154:11015/',					//支付
+		'VisaEngine' 			=> 'http://192.168.3.154:11007/',					//引擎
+		'CustomerService' 		=> 'http://192.168.3.154:11014/',					//客服
+		'ConfigManageSystem' 	=> 'http://192.168.3.154:11016/',					//cms
+		'TransferSystem' 		=> 'http://192.168.3.154:11018/',					//物流
+		'AtsPush'   			=> 'http://192.168.3.213:80/',						//推送
+		'Passport' 				=> 'http://apippt.baicheng.com/',					//会员
+		'WenDa' 				=> 'http://wenda.baicheng.com/',					//问答
+		'PayCenterManager' 		=> 'http://192.168.3.154:11025/',					//支付中心管理后台
+		'TeMai'					=> 'http://temai.internal.baicheng.com/'			//特卖
 	);
 	
 	public function getNode($node)
